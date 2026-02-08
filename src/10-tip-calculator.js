901a415af32dcea5ab743b2a2fe54df3e57a9c77
shubh ujala <shubhujala.code@gmail.com>
2026-02-08 16:10:24 +0530

    10 question done

--- src/10-tip-calculator.js
+++ src/10-tip-calculator.js
@@ -31,4 +31,48 @@
  */
 export function calculateTip(billAmount, serviceRating) {
   // Your code here
+  if(billAmount <=0){
+    return null;
+  }
+  if(serviceRating<1 || serviceRating>5 || !Number.isInteger(serviceRating)){
+    return null;
+  }
+
+  let returnObj = {
+    tipPercentage:0,
+    tipAmount:0,
+    totalAmount:0,
+  }
+
+  if(serviceRating == 1){
+    var tip_am = billAmount * (0.05);
+    returnObj.tipPercentage = 5;
+    returnObj.tipAmount = tip_am;
+    returnObj.totalAmount = tip_am+billAmount;
+  }else if(serviceRating == 2){
+    var tip_am = billAmount * (0.1);
+    returnObj.tipPercentage = 10;
+    returnObj.tipAmount = tip_am;
+    returnObj.totalAmount = tip_am+billAmount;
+  
+  }else if(serviceRating == 3){
+    var tip_am = billAmount * (0.15);
+    returnObj.tipPercentage = 15;
+    returnObj.tipAmount = tip_am;
+    returnObj.totalAmount = tip_am+billAmount;
+
+  }else if(serviceRating == 4){
+    var tip_am = billAmount * (0.2);
+    returnObj.tipPercentage = 20;
+    returnObj.tipAmount = tip_am;
+    returnObj.totalAmount = tip_am+billAmount;
+  
+  }else{
+    var tip_am = billAmount * (0.25);
+    returnObj.tipPercentage = 25;
+    returnObj.tipAmount = tip_am;
+    returnObj.totalAmount = tip_am+billAmount;
+    
+  }
+  return returnObj;
 }
